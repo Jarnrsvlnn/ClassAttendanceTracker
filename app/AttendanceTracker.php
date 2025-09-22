8adfda4031d5397795c822c74b43d50e7a029709
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-09-22 17:45:00 +0800

    Added functions inside the AttendanceTracker class that reads and gets students and attendance data

--- app/AttendanceTracker.php
+++ app/AttendanceTracker.php
@@ -1,24 +1,48 @@
 <?php
 
-
 class AttendanceTracker
 {
-    private string $studentFile;
-    private string $attendanceFile;
+    private array $studentsData = [];
+    private array $attendanceData = [];
 
     // gets the csv file to read from
 
     public function __construct(string $studentFile, string $attendanceFile)
     {
-        $this->studentFile = $studentFile;
-        $this->attendanceFile = $attendanceFile;
+        $this->studentsData = $this->readStudentData($studentFile);
+        $this->attendanceData = $this->readAttendanceData($attendanceFile);
     }
 
     // reads the file passed and gets the student data
-    public function readStudentData(): AttendanceTracker
+    public function readStudentData(string $studentData): array
     {
+        $studentsDataArray = [];
+        if (($studentFile = fopen($studentData, 'r')) !== false) {
+            while (($studentCsv = fgetcsv($studentFile, 0, ",", '"', "\\")) !== false) {
+                $studentsDataArray[] = $studentCsv;
+            }
+        }
+        return $studentsDataArray;
+    }
 
+    public function readAttendanceData(string $attendanceData): array
+    {
+        $attendanceDataArray = [];
+        if (($attendanceFile = fopen($attendanceData, 'r')) !== false) {
+            while (($attendanceCsv = fgetcsv($attendanceFile, 0, ",", '"', "\\")) !== false) {
+                $attendanceDataArray[] = $attendanceCsv;
+            }
+        }
+        return $attendanceDataArray;
+    }
 
-        return $this;
+    public function getStudentsData(): array
+    {
+        return $this->studentsData;
+    }
+
+    public function getAttendanceData(): array
+    {
+        return $this->attendanceData;
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,14 +1,24 @@
 <?php
 
-$rootDirectory = dirname(__DIR__, 2) . DIRECTORY_SEPARATOR;
+$rootDirectory = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
 define('APP_PATH', $rootDirectory . 'app' . DIRECTORY_SEPARATOR);
 define('DATA_PATH', $rootDirectory . 'data' . DIRECTORY_SEPARATOR);
-define('VIEWS_PATJH', $rootDirectory . 'views' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $rootDirectory . 'views' . DIRECTORY_SEPARATOR);
+
 
 require_once APP_PATH . "Student.php";
 require_once APP_PATH . "Attendance.php";
 require_once APP_PATH . "helpers.php";
-require_once DATA_PATH . "attendance_data.csv";
-require_once DATA_PATH . "students_data.csv";
+require_once APP_PATH . 'AttendanceTracker.php';
 
+
+$track = new AttendanceTracker(DATA_PATH . 'students_data.csv', DATA_PATH . 'attendance_data.csv');
+
+echo '<pre>';
+print_r($track->getStudentsData());
+echo '<pre/>';
+
+echo '<pre>';
+print_r($track->getAttendanceData());
+echo '<pre/>';
